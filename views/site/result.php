3c961378ff0a6ccbb0134203ffc7c3169a99c8b3
Your Name <oluwatosin.olamilekan@54gene.com>
2019-10-03 15:13:26 +0100

    last changes

--- views/site/result.php
+++ views/site/result.php
@@ -6,21 +6,16 @@
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <!-- <h1>Congratulations!</h1> -->
-        <p class="lead">Total Result  of all polling unit under  each particular Local Government</p>
-
-    </div>
 
     <div class="body-content">
 
         <div class="row">
-            <ul class="list-group">
+            <div class="col-md-3">
                 <?php foreach ($results as $row) { ?>
                     <li class='list-group-item'><?= $row['party_abbreviation'] ?> <span class='badge'><?= $row['party_score'] ?></span></li>
                 <?php } ?>
                 
-            </ul>
+            </div>
         </div>
 
     </div>
